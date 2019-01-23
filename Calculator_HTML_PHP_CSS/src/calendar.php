687780d53aa9f899fc78dc27d60e3bf01a48da27
Paula Banks <bpaula92@gmail.com>
2019-01-23 12:30:06 +0000

    Renamed and prepared the file for a basic calendar build

---
+++ Calculator_HTML_PHP_CSS/src/calendar.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <!--Meta information goes here-->
+
+        <meta charset="UTF-8"> <!--charset attribute specifies the character encoding-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--gives the browser instructions on how to control the page's dimensions and scaling.-->
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"><!--Read more about browser support, especially for IE--> 
+        <meta name="description" content=""><!--Write here your page description-->
+        <meta name="author" content=""><!--Who is the author?-->
+        <title>Basic Calculator</title><!--Title that will appear in a browser tag-->
+
+        <!--User/Bootstrap CSS, fonts, icons-->
+
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="css/calculator.css"><!--Add linking to your css file-->
+        <link href="https://fonts.googleapis.com/css?family=Coiny" rel="stylesheet"><!--Really good fonts source: https://fonts.google.com/-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"><!--Adding cool icons to the site from https://fontawesome.com/icons-->
+
+    </head>
+    <body>
+
+        <nav class="nav"> 
+            <ul class="nav-list"> 
+                <li class="nav-list-item"> 
+                    <a class="nav-list-item-link" href="index.php">ABOUT ME</a> 
+                </li> 
+                <li class="nav-list-item"> 
+                    <a class="nav-list-item-link" href="/Favourites.php">FAVOURITES</a> 
+                </li> 
+                <li class="nav-list-item"> 
+                    <a class="nav-list-item-link" href="Calculator.php">CALCULATOR</a> 
+                </li> 
+            </ul>
+        </nav>
+
+        <header>
+            <h1 class="page-title">CALENDAR</h1>
+            <p class="page-intro">Calendar build in PHP, HTML and CSS</p>
+        </header>
+
+        <main class="page-container">
+
+        </main>
+
+        <footer></footer>
+    </body>
+</html>
