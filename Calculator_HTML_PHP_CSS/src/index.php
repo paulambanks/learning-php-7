b539ca114ed45006e465fbb0b54e22deba9f7753
Paula Banks <bpaula92@gmail.com>
2019-01-24 20:00:18 +0000

    Updated index.php

--- Calculator_HTML_PHP_CSS/src/index.php
+++ Calculator_HTML_PHP_CSS/src/index.php
@@ -11,13 +11,13 @@
         <?php include ("include/navigation.php"); ?>      
 
         <header>
-            <h1 class="page-title">CALCULATOR</h1>
-            <p class="page-intro">This is a very simple calculator build only with PHP, CSS and HTML Feel free to have some fun with it. The calculator numerical input will be filtered so make sure you're using numbers. Also... make sure you are not dividing by 0.</p>
+            <h1 class="page-title">WELCOME</h1>
+            <p class="page-intro">This is my little PHP, HTML and CSS playground.</p>
         </header>
 
         <main class="page-container">
             
-            <h1>Hello World!!!!</h1> 
+            
       
         </main>
 
